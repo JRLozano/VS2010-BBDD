f88645f95bd18209bb0720bb1fc14c389484b9cf
unknown <jreal94@gmail.com>
2014-12-24 13:37:10 +0100

    Aplicacion de prueba para trabajar con bases de datos desde VS2010

---
+++ PruebaBBDD/PruebaBBDD/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Consultar = New System.Windows.Forms.Button()
+        Me.Añadir = New System.Windows.Forms.Button()
+        Me.Modificar = New System.Windows.Forms.Button()
+        Me.Eliminar = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Consultar
+        '
+        Me.Consultar.Location = New System.Drawing.Point(124, 79)
+        Me.Consultar.Name = "Consultar"
+        Me.Consultar.Size = New System.Drawing.Size(75, 23)
+        Me.Consultar.TabIndex = 0
+        Me.Consultar.Text = "Consultar"
+        Me.Consultar.UseVisualStyleBackColor = True
+        '
+        'Añadir
+        '
+        Me.Añadir.Location = New System.Drawing.Point(124, 108)
+        Me.Añadir.Name = "Añadir"
+        Me.Añadir.Size = New System.Drawing.Size(75, 23)
+        Me.Añadir.TabIndex = 1
+        Me.Añadir.Text = "Añadir"
+        Me.Añadir.UseVisualStyleBackColor = True
+        '
+        'Modificar
+        '
+        Me.Modificar.Location = New System.Drawing.Point(124, 137)
+        Me.Modificar.Name = "Modificar"
+        Me.Modificar.Size = New System.Drawing.Size(75, 23)
+        Me.Modificar.TabIndex = 2
+        Me.Modificar.Text = "Modificar"
+        Me.Modificar.UseVisualStyleBackColor = True
+        '
+        'Eliminar
+        '
+        Me.Eliminar.Location = New System.Drawing.Point(124, 166)
+        Me.Eliminar.Name = "Eliminar"
+        Me.Eliminar.Size = New System.Drawing.Size(75, 23)
+        Me.Eliminar.TabIndex = 3
+        Me.Eliminar.Text = "Eliminar"
+        Me.Eliminar.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 81)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 110)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 139)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Eliminar)
+        Me.Controls.Add(Me.Modificar)
+        Me.Controls.Add(Me.Añadir)
+        Me.Controls.Add(Me.Consultar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Consultar As System.Windows.Forms.Button
+    Friend WithEvents Añadir As System.Windows.Forms.Button
+    Friend WithEvents Modificar As System.Windows.Forms.Button
+    Friend WithEvents Eliminar As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+
+End Class
---
+++ PruebaBBDD/PruebaBBDD/Form1.vb
@@ -0,0 +1,116 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conector As New MySqlConnection("server=localhost;user=ejemplo;password=ejemplo;database=ejemplo;port=3306")
+    Dim comando As String
+    Dim reader As MySqlDataReader
+
+    Function Validar(ByVal numDep As Integer) As Boolean
+        comando = "SELECT * FROM Departamentos WHERE dept_no = @num"
+        Dim cmd As New MySqlCommand(comando, conector)
+        conector.Open()
+        cmd.Parameters.Add("@num", MySqlDbType.Int16).Value = numDep
+        reader = cmd.ExecuteReader
+        Validar = False
+
+        If reader.Read Then
+            reader.Close()
+            Validar = True
+        End If
+
+        conector.Close()
+        Return Validar
+    End Function
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Consultar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Consultar.Click
+        comando = "SELECT dnombre, loc FROM Departamentos WHERE dept_no = @numDep"
+        Dim cmd As New MySqlCommand(comando, conector)
+        conector.Open()
+        cmd.Parameters.Add("@numDep", MySqlDbType.Int16).Value = TextBox1.Text
+        reader = cmd.ExecuteReader
+
+        If reader.Read Then
+            TextBox2.Text = reader.GetString(0)
+            TextBox3.Text = reader.GetString(1)
+        Else
+            MsgBox("El departamento no existe")
+        End If
+        reader.Close()
+        conector.Close()
+
+    End Sub
+
+    Private Sub Añadir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Añadir.Click
+        'comando = "SELECT dept_no FROM Departamentos WHERE dept_no = @numDep"
+        'Dim cmd As New MySqlCommand(comando, conector)
+
+        'cmd.Parameters.Add("@numDep", MySqlDbType.Int16).Value = TextBox1.Text
+        'reader = cmd.ExecuteReader
+
+        If Validar(TextBox1.Text) Then
+            MsgBox("El departamento ya existe")
+        Else
+            reader.Close()
+            Dim comando As String = "INSERT INTO Departamentos VALUES(@numDep, @nom, @loc)"
+            Dim cmd As New MySqlCommand(comando, conector)
+            conector.Open()
+            cmd.Parameters.Add("@numDep", MySqlDbType.Int16).Value = TextBox1.Text
+            cmd.Parameters.Add("@nom", MySqlDbType.String).Value = TextBox2.Text
+            cmd.Parameters.Add("@loc", MySqlDbType.String).Value = TextBox3.Text
+            cmd.ExecuteNonQuery()
+            MsgBox("Departamento Introducido")
+        End If
+
+
+        conector.Close()
+    End Sub
+
+    Private Sub Eliminar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Eliminar.Click
+        comando = "SELECT dept_no FROM Departamentos WHERE dept_no = @numDep"
+        Dim cmd As New MySqlCommand(comando, conector)
+        conector.Open()
+        cmd.Parameters.Add("@numDep", MySqlDbType.Int16).Value = TextBox1.Text
+        reader = cmd.ExecuteReader
+
+        If reader.Read Then
+            Try
+                reader.Close()
+                Dim comando2 As String = "DELETE FROM Departamentos WHERE dept_no = @numDep"
+                Dim cmd2 As New MySqlCommand(comando2, conector)
+                cmd2.Parameters.Add("@numDep", MySqlDbType.Int16).Value = TextBox1.Text
+                cmd2.ExecuteNonQuery()
+
+                MsgBox("Departamentos Eliminado")
+            Catch ex As MySqlException
+                MsgBox(ex.Message)
+            End Try
+           
+
+
+        Else
+            MsgBox("El departamento no existe")
+        End If
+
+        ' reader.Close()
+        conector.Close()
+
+    End Sub
+
+    Private Sub Modificar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Modificar.Click
+        comando = "UPDATE Departamentos SET dnombre = @nom, loc = @loc WHERE dept_no = @numDep"
+        Dim cmd As New MySqlCommand(comando, conector)
+        conector.Open()
+        cmd.Parameters.Add("@numDep", MySqlDbType.Int16).Value = TextBox1.Text
+        cmd.Parameters.Add("@nom", MySqlDbType.String).Value = TextBox2.Text
+        cmd.Parameters.Add("@loc", MySqlDbType.String).Value = TextBox3.Text
+
+        cmd.ExecuteNonQuery()
+        MsgBox("El Departamento ha sido modificado")
+
+        conector.Close()
+    End Sub
+End Class
