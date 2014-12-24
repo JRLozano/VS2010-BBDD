09df58e8da6c2ce7732ec8cd4c4a8a8adbc5b3fc
unknown <jreal94@gmail.com>
2014-12-24 15:48:15 +0100

    Primer programa en C#, el cual trabaja con BBDD MySQL

---
+++ BBDDC/BBDDC/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Consultar = new System.Windows.Forms.Button();
+            this.Añadir = new System.Windows.Forms.Button();
+            this.Modificar = new System.Windows.Forms.Button();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Consultar
+            // 
+            this.Consultar.Location = new System.Drawing.Point(160, 47);
+            this.Consultar.Name = "Consultar";
+            this.Consultar.Size = new System.Drawing.Size(75, 23);
+            this.Consultar.TabIndex = 0;
+            this.Consultar.Text = "Consultar";
+            this.Consultar.UseVisualStyleBackColor = true;
+            this.Consultar.Click += new System.EventHandler(this.Consultar_Click);
+            // 
+            // Añadir
+            // 
+            this.Añadir.Location = new System.Drawing.Point(160, 76);
+            this.Añadir.Name = "Añadir";
+            this.Añadir.Size = new System.Drawing.Size(75, 23);
+            this.Añadir.TabIndex = 1;
+            this.Añadir.Text = "Añadir";
+            this.Añadir.UseVisualStyleBackColor = true;
+            this.Añadir.Click += new System.EventHandler(this.Añadir_Click);
+            // 
+            // Modificar
+            // 
+            this.Modificar.Location = new System.Drawing.Point(160, 106);
+            this.Modificar.Name = "Modificar";
+            this.Modificar.Size = new System.Drawing.Size(75, 23);
+            this.Modificar.TabIndex = 2;
+            this.Modificar.Text = "Modificar";
+            this.Modificar.UseVisualStyleBackColor = true;
+            this.Modificar.Click += new System.EventHandler(this.Modificar_Click);
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.Location = new System.Drawing.Point(160, 135);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.Eliminar.TabIndex = 3;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.UseVisualStyleBackColor = true;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(24, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Eliminar);
+            this.Controls.Add(this.Modificar);
+            this.Controls.Add(this.Añadir);
+            this.Controls.Add(this.Consultar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Consultar;
+        private System.Windows.Forms.Button Añadir;
+        private System.Windows.Forms.Button Modificar;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ BBDDC/BBDDC/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication1{
+    public partial class Form1 : Form{
+        MySqlConnection conector = new MySqlConnection("server=localhost;user=ejemplo;password=ejemplo;database=ejemplo");
+        String comando;
+        MySqlCommand cmd;
+        MySqlDataReader reader;
+
+        public Form1(){            
+            InitializeComponent();
+        }
+
+        public Boolean Validar(Int16 num)
+        {
+            Boolean existe = false;
+
+            comando = "SELECT * FROM Departamentos WHERE dept_no = @num";
+            cmd = new MySqlCommand(comando, conector);
+            conector.Open();
+            cmd.Parameters.Add("@num", MySqlDbType.Int16).Value = num;
+            reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                reader.Close();
+                existe = true;
+            }
+
+            conector.Close();
+            return existe;
+        }
+
+        public void Limpiar()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void Consultar_Click(object sender, EventArgs e){
+            comando = "SELECT * FROM Departamentos WHERE dept_no = @numDep";
+            cmd = new MySqlCommand(comando, conector);
+            conector.Open();
+            cmd.Parameters.Add("@numDep", MySqlDbType.Byte).Value = textBox1.Text;
+            reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                textBox2.Text = reader.GetString(1);
+                textBox3.Text = reader.GetString(2);
+            }
+            else
+            {
+                MessageBox.Show("El departamento no existe", "Error");
+            }
+            conector.Close();
+        }
+
+        private void Añadir_Click(object sender, EventArgs e)
+        {
+            
+
+            if (!Validar(Convert.ToInt16(textBox1.Text)))
+            {
+                comando = "INSERT INTO Departamentos VALUES(@numDep, @nom, @loc)";
+                cmd = new MySqlCommand(comando, conector);
+                conector.Open();
+                cmd.Parameters.Add("@numDep", MySqlDbType.Int16).Value = textBox1.Text;
+                cmd.Parameters.Add("@nom", MySqlDbType.String).Value = textBox2.Text;
+                cmd.Parameters.Add("@loc", MySqlDbType.String).Value = textBox3.Text;
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Departamento introducido", "Exito");
+                Limpiar();
+            }
+            else
+            {
+                MessageBox.Show("El departamento ya existe", "Error");
+            }
+            conector.Close();
+        }
+
+        private void Eliminar_Click(object sender, EventArgs e)
+        {
+            if (Validar(Convert.ToInt16(textBox1.Text)))
+            {
+                try
+                {
+                    comando = "DELETE FROM Departamentos WHERE dept_no = @numDep";
+                    cmd = new MySqlCommand(comando, conector);
+                    conector.Open();
+                    cmd.Parameters.Add("@numDep", MySqlDbType.Int16).Value = textBox1.Text;
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Departamento eliminado", "Exito");
+                    Limpiar();
+                }
+                catch (MySqlException me)
+                {
+                    MessageBox.Show(me.Message, "");
+                }
+            }
+            else {
+                MessageBox.Show("El departamento no existe", "Error");
+            }
+
+            conector.Close();
+        }
+
+        private void Modificar_Click(object sender, EventArgs e)
+        {
+            if (Validar(Convert.ToInt16(textBox1.Text)))
+            {
+                comando = "UPDATE Departamentos SET dnombre = @nom, loc = @loc WHERE dept_no = @numDep";
+                cmd = new MySqlCommand(comando, conector);
+                conector.Open();
+                cmd.Parameters.Add("@numDep", MySqlDbType.Int16).Value = textBox1.Text;
+                cmd.Parameters.Add("@nom", MySqlDbType.String).Value = textBox2.Text;
+                cmd.Parameters.Add("@loc", MySqlDbType.String).Value = textBox3.Text;
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Departamento modificado", "Exito");
+                Limpiar();
+            }
+            else
+            {
+                MessageBox.Show("El departamento no existe");            
+            }
+            conector.Close();
+        }    
+    }
+}
